1ec0a1afd43732d3992d15b537f3345bf4c8fb66
David Haas <davidhaas@Davids-MacBook-Air.local>
2020-04-10 09:32:04 -0400

    lesson 9 JSX expressions

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,10 +6,48 @@ console.log('App.js is running!');
 // Running watch will automatically change this in scripts
 // which is the converted code from Babel
 var template = React.createElement(
-  'h1',
-  null,
-  'Indecision App'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Indecision App'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Some word stuff here'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Some more word stuff here'
+        )
+    )
+);
+
+var template2 = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'David Haas'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age: 54'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location: Southern Shores, NC'
+    )
 );
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(template2, appRoot);
--- src/app.js
+++ src/app.js
@@ -3,7 +3,24 @@ console.log('App.js is running!');
 // JSX - JavaScript XML
 // Running watch will automatically change this in scripts
 // which is the converted code from Babel
-var template = <h1>Indecision App</h1>;
+var template = (
+    <div>
+        
+        <h1>Indecision App</h1>
+        <ol>
+        <li>Some word stuff here</li>
+        <li>Some more word stuff here</li>
+        </ol>
+    </div>
+);
+
+var template2 = (
+<div>
+    <h1>David Haas</h1>
+    <p>Age: 54</p>
+    <p>Location: Southern Shores, NC</p>
+    </div>
+);
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(template2, appRoot);
